d2ae01ec019be3998a7f9c903cbf9ef151ee5ea5
Kimmo Forss <kimforss@microsoft.com>
2025-01-14 23:42:06 +0200

    Update network logical name variable and add app configuration for SAP library

---
+++ deploy/terraform/terraform-units/modules/sap_library/app-configuration.tf
@@ -0,0 +1,49 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+#######################################4#######################################8
+#                                                                              #
+#                          Azure App Configuration                             #
+#                                                                              #
+#######################################4#######################################8
+
+data  "azurerm_app_configuration" "app_config" {
+  provider                             = azurerm.deployer
+  name                                 = var.deployer_tfstate.deployer_app_config_name
+  resource_group_name                  = var.deployer_tfstate.created_resource_group_name
+}
+
+
+resource "azurerm_app_configuration_key" "library_app_configuration_keys" {
+  for_each               = local.configuration_values
+  provider               = azurerm.deployer
+  configuration_store_id = data.azurerm_app_configuration.app_config.id
+  key                    = each.key
+  label                  = each.value.label
+  value                  = each.value.value
+}
+
+locals {
+
+  prefix                               = replace(var.state_filename_prefix, format("-%", var.deployer_tfstate.network_logical_name), "")
+  configuration_values                 = {
+                                          format("%s_LibraryStateFileName", local.prefix) = {
+                                            label = local.resource_group_exists ? ( data.azurerm_resource_group.library[0].name) : ( azurerm_resource_group.library[0].name )
+                                            value = format("%s-INFRASTRUCTURE.terraform.tfstate",var.state_filename_prefix)
+                                          }
+                                          format("%s_TerraformRemoteStateStorageAccountId", local.prefix) = {
+                                            label = local.resource_group_exists ? ( data.azurerm_resource_group.library[0].name) : ( azurerm_resource_group.library[0].name )
+                                            value = local.sa_tfstate_exists ? (
+                                                     data.azurerm_storage_account.storage_tfstate[0].id) : (
+                                                     try(azurerm_storage_account.storage_tfstate[0].id, "")
+                                                   )
+                                          }
+                                          format("%s_SAPLibraryStorageAccountId", local.prefix) = {
+                                            label = local.resource_group_exists ? ( data.azurerm_resource_group.library[0].name) : ( azurerm_resource_group.library[0].name )
+                                            value = local.sa_sapbits_exists ? (
+                                                     data.azurerm_storage_account.storage_sapbits[0].id) : (
+                                                     try(azurerm_storage_account.storage_sapbits[0].id, "")
+                                                   )
+                                          }
+                                        }
+}
