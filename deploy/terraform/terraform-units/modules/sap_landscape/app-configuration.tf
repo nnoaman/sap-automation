71314e4ef6dac9dc8e7db32a812a30e75ed94d93
Kimmo Forss <kimforss@microsoft.com>
2025-02-22 14:32:37 +0200

    Refactor app configuration keys for improved structure and clarity

--- deploy/terraform/terraform-units/modules/sap_landscape/app-configuration.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/app-configuration.tf
@@ -13,43 +13,35 @@ data  "azurerm_app_configuration" "app_config" {
   resource_group_name                  = local.app_config_resource_group_name
 }
 
-
-resource "azurerm_app_configuration_key" "workload_app_configuration_keys" {
-  for_each                             = local.configuration_values
+resource "azurerm_app_configuration_key" "KeyVaultResourceId" {
   provider                             = azurerm.deployer
   configuration_store_id               = data.azurerm_app_configuration.app_config.id
-  key                                  = each.key
-  label                                = each.value.label
-  value                                = each.value.value
-  content_type                         = each.value.content_type
+  key                                  = format("%s_KeyVaultResourceId", var.naming.prefix.WORKLOAD_ZONE)
+  label                                = var.naming.prefix.WORKLOAD_ZONE
+  value                                = length(try(var.key_vault.keyvault_id_for_system_credentials, "")) > 0 ? (
+                                              try(data.azurerm_key_vault.kv_user[0].id, "")) : (
+                                              try(azurerm_key_vault.kv_user[0].id, "")
+                                            )
+  content_type                         = "text/id"
+  type                                 = "kv"
 }
+resource "azurerm_app_configuration_key" "VirtualNetworkResourceId" {
+  provider                             = azurerm.deployer
+  configuration_store_id               = data.azurerm_app_configuration.app_config.id
+  key                                  = format("%s_VirtualNetworkResourceId", var.naming.prefix.WORKLOAD_ZONE)
+  label                                = var.naming.prefix.WORKLOAD_ZONE
+  value                                = local.SAP_virtualnetwork_exists ? (
+                                                try(data.azurerm_virtual_network.vnet_sap[0].id, "")) : (
+                                                try(azurerm_virtual_network.vnet_sap[0].id, "")
+                                              )
+  content_type                         = "text/id"
+  type                                 = "kv"
+}
+
 
 locals {
 
   parsed_id                           = provider::azurerm::parse_resource_id(coalesce(var.infrastructure.application_configuration_id, var.deployer_tfstate.deployer_app_config_id))
   app_config_name                     = local.parsed_id["resource_name"]
   app_config_resource_group_name      = local.parsed_id["resource_group_name"]
-  configuration_values                = {
-                                          format("%s_KeyVaultResourceId", var.naming.prefix.WORKLOAD_ZONE) = {
-                                            label = var.naming.prefix.WORKLOAD_ZONE
-                                            value = length(try(var.key_vault.keyvault_id_for_system_credentials, "")) > 0 ? (
-                                              try(data.azurerm_key_vault.kv_user[0].id, "")) : (
-                                              try(azurerm_key_vault.kv_user[0].id, "")
-                                            )
-                                            label = var.naming.prefix.WORKLOAD_ZONE
-                                            value = length(try(var.key_vault.keyvault_id_for_system_credentials, "")) > 0 ? (
-                                              try(data.azurerm_key_vault.kv_user[0].id, "")) : (
-                                              try(azurerm_key_vault.kv_user[0].id, "")
-                                            )
-                                            content_type = "text/id"
-                                            format("%s_VirtualNetworkResourceId", var.naming.prefix.WORKLOAD_ZONE) = {
-                                              label = var.naming.prefix.WORKLOAD_ZONE
-                                              value = local.SAP_virtualnetwork_exists ? (
-                                                try(data.azurerm_virtual_network.vnet_sap[0].id, "")) : (
-                                                try(azurerm_virtual_network.vnet_sap[0].id, "")
-                                              )
-                                              content_type = "text/id"
-                                            }
-                                          }
-                                        }
-}
+  }
