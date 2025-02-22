eed88ec8ecb1b9e7d45d7241324806a9187a469f
Kimmo Forss <kimforss@microsoft.com>
2025-02-22 11:31:04 +0200

    Refactor app configuration keys for improved clarity and structure

--- deploy/terraform/terraform-units/modules/sap_library/app-configuration.tf
+++ deploy/terraform/terraform-units/modules/sap_library/app-configuration.tf
@@ -13,15 +13,54 @@ data  "azurerm_app_configuration" "app_config" {
   resource_group_name                  = local.app_config_resource_group_name
 }
 
-
-resource "azurerm_app_configuration_key" "library_app_configuration_keys" {
-  for_each                             = local.configuration_values
+resource "azurerm_app_configuration_key" "libraryStateFileName" {
   provider                             = azurerm.deployer
   configuration_store_id               = data.azurerm_app_configuration.app_config.id
-  key                                  = each.key
-  label                                = each.value.label
-  value                                = each.value.value
-  content_type                         = each.value.content_type
+  key                                  = format("%s_LibraryStateFileName", var.state_filename_prefix)
+  label                                = var.state_filename_prefix
+  value                                = format("%s-SAP_LIBRARY.terraform.tfstate",var.naming.prefix.LIBRARY)
+  content_type                         = "text/plain"
+  type                                 = "kv"
+}
+
+
+resource "azurerm_app_configuration_key" "terraformRemoteStateStorageAccountId" {
+  provider                             = azurerm.deployer
+  configuration_store_id               = data.azurerm_app_configuration.app_config.id
+  key                                  = format("%s_TerraformRemoteStateStorageAccountId", var.state_filename_prefix)
+  label                                = var.state_filename_prefix
+  value                                = local.sa_tfstate_exists ? (
+                                                            data.azurerm_storage_account.storage_tfstate[0].id) : (
+                                                            try(azurerm_storage_account.storage_tfstate[0].id, "")
+                                                          )
+  content_type                         = "text/id"
+  type                                 = "kv"
+}
+
+
+resource "azurerm_app_configuration_key" "SAPLibraryStorageAccountId" {
+  provider                             = azurerm.deployer
+  configuration_store_id               = data.azurerm_app_configuration.app_config.id
+  key                                  = format("%s_SAPLibraryStorageAccountId", var.state_filename_prefix)
+  label                                = var.state_filename_prefix
+  value                                = local.sa_tfstate_exists ? (
+                                                            data.azurerm_storage_account.storage_sapbits[0].id) : (
+                                                            try(azurerm_storage_account.storage_sapbits[0].id, "")
+                                                          )
+  content_type                         = "text/id"
+  type                                 = "kv"
+}
+
+resource "azurerm_app_configuration_key" "SAPMediaPath" {
+  provider                             = azurerm.deployer
+  configuration_store_id               = data.azurerm_app_configuration.app_config.id
+  key                                  = format("%s_SAPMediaPath", var.state_filename_prefix)
+  label                                = var.state_filename_prefix
+  value                                = format("https://%s.blob.core.windows.net/%s", length(var.storage_account_sapbits.arm_id) > 0 ?
+                                                             split("/", var.storage_account_sapbits.arm_id)[8] : local.sa_sapbits_name,
+                                                             var.storage_account_sapbits.sapbits_blob_container.name)
+  content_type                         = "text/plain"
+  type                                 = "kv"
 }
 
 locals {
@@ -29,37 +68,4 @@ locals {
   parsed_id                            = provider::azurerm::parse_resource_id(coalesce(var.deployer.application_configuration_id,var.deployer_tfstate.deployer_app_config_id))
   app_config_name                      = local.parsed_id["resource_name"]
   app_config_resource_group_name       = local.parsed_id["resource_group_name"]
-  configuration_values                 = {
-                                          format("%s_LibraryStateFileName", var.state_filename_prefix) = {
-                                            label        = var.state_filename_prefix
-                                            value        = format("%s-SAP_LIBRARY.terraform.tfstate",var.naming.prefix.LIBRARY)
-                                            content_type = "text/plain"
-                                          }
-                                          format("%s_TerraformRemoteStateStorageAccountId", var.state_filename_prefix) = {
-                                            label        = var.state_filename_prefix
-                                            value        = local.sa_tfstate_exists ? (
-                                                            data.azurerm_storage_account.storage_tfstate[0].id) : (
-                                                            try(azurerm_storage_account.storage_tfstate[0].id, "")
-                                                          )
-                                            content_type = "text/id"
-                                          }
-                                          format("%s_SAPLibraryStorageAccountId", var.state_filename_prefix) = {
-                                            label        = var.state_filename_prefix
-                                            value        = local.sa_sapbits_exists ? (
-                                                            data.azurerm_storage_account.storage_sapbits[0].id) : (
-                                                            try(azurerm_storage_account.storage_sapbits[0].id, "")
-                                                          )
-                                            content_type = "text/id"
-                                          }
-
-                                          format("%s_SAPMediaPath", var.state_filename_prefix) = {
-                                            label        = var.state_filename_prefix
-                                            value        = format("https://%s.blob.core.windows.net/%s", length(var.storage_account_sapbits.arm_id) > 0 ?
-                                                             split("/", var.storage_account_sapbits.arm_id)[8] : local.sa_sapbits_name,
-                                                             var.storage_account_sapbits.sapbits_blob_container.name)
-
-                                            content_type = "text/id"
-                                          }
-
-                                        }
 }
