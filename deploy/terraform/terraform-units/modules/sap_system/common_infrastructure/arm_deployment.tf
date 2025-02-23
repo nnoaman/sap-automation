36cb192f8c3c41ca81873ffca68a0385de50ac96
Kimmo Forss <kimforss@microsoft.com>
2025-02-23 16:02:01 +0200

    Refactor ARM deployment template for SAP system to include schema and parameters for improved structure

--- deploy/terraform/terraform-units/modules/sap_system/common_infrastructure/arm_deployment.tf
+++ deploy/terraform/terraform-units/modules/sap_system/common_infrastructure/arm_deployment.tf
@@ -3,11 +3,23 @@ resource "azurerm_resource_group_template_deployment" "sap_system" {
   provider                             = azurerm.main
   name                                 = format("SDAF-%s-%s-%s", var.database.platform, var.application_tier.sid, local.deployment_type)
   resource_group_name                  = local.resource_group_exists ? (
-                                                           data.azurerm_resource_group.resource_group[0].name) : (
-                                                           azurerm_resource_group.resource_group[0].name
-                                                         )
+                                           data.azurerm_resource_group.resource_group[0].name) : (
+                                           azurerm_resource_group.resource_group[0].name
+                                         )
   deployment_mode                      = "Incremental"
 
-  template_content                     = jsonencode(file(format("%s%s", path.module, "/templates/deployment.json")))
+  template_content                     = jsonencode(
+                                           {
+                                             "$schema" : "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+                                             "contentVersion" : "1.0.0.0",
+                                             "parameters" : {
+                                               "Deployment" : {
+                                                 "type" : "string",
+                                                 "defaultValue" : "SAP System"
+                                               }
+                                             },
+                                             "resources" : []
+                                           }
+                                         )
 
 }
