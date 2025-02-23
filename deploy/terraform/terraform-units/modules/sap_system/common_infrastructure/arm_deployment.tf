de13d99f519a6aa91fd32bd8fc568a4a05377213
Kimmo Forss <kimforss@microsoft.com>
2025-02-23 15:54:54 +0200

    Rename azurerm_template_deployment to azurerm_resource_group_template_deployment and update variable references for improved clarity

--- deploy/terraform/terraform-units/modules/sap_system/common_infrastructure/arm_deployment.tf
+++ deploy/terraform/terraform-units/modules/sap_system/common_infrastructure/arm_deployment.tf
@@ -1,13 +1,13 @@
 
-resource "azurerm_template_deployment" "sap_system" {
+resource "azurerm_resource_group_template_deployment" "sap_system" {
   provider                             = azurerm.main
-  name                                 = format("SDAF-%s-%s-%s", var.platform, var.sid, var.environment, local.deployment_type)
+  name                                 = format("SDAF-%s-%s-%s", var.database.platform, var.application_tier.sid, local.deployment_type)
   resource_group_name                  = local.resource_group_exists ? (
                                                            data.azurerm_resource_group.resource_group[0].name) : (
                                                            azurerm_resource_group.resource_group[0].name
                                                          )
   deployment_mode                      = "Incremental"
 
-  template_body                        = file(format("%s%s", path.module, "/templates/deployment.json"))
+  template_content                     = jsonencode(file(format("%s%s", path.module, "/templates/deployment.json")))
 
 }
