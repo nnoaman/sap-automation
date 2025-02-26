48a17f35c2742e9882db338a9367d572e97b3322
Kimmo Forss <kimforss@microsoft.com>
2025-02-27 00:27:45 +0200

    Add LogonToAzure function for Azure authentication and adjust script sourcing

--- deploy/scripts/helpers/script_helpers.sh
+++ deploy/scripts/helpers/script_helpers.sh
@@ -683,3 +683,33 @@ function validate_key_vault {
 	return $return_value
 
 }
+
+function LogonToAzure() {
+	local useMSI=$1
+	local subscriptionId=$ARM_SUBSCRIPTION_ID
+
+	if [ "$useMSI" != "true" ]; then
+		echo "Deployment credentials:              Service Principal"
+		echo "Deployment credential ID (SPN):      $ARM_CLIENT_ID"
+		unset ARM_USE_MSI
+		az login --service-principal --username "$ARM_CLIENT_ID" --password="$ARM_CLIENT_SECRET" --tenant "$ARM_TENANT_ID" --output none
+		echo "Logged on as:"
+		az account show --query user --output yaml
+		TF_VAR_use_spn=true
+		export TF_VAR_use_spn
+
+	else
+		echo "Deployment credentials:              Managed Service Identity"
+		source "/etc/profile.d/deploy_server.sh"
+		TF_VAR_use_spn=false
+		export TF_VAR_use_spn
+
+		# sourcing deploy_server.sh overwrites ARM_SUBSCRIPTION_ID with control plane subscription id
+		# ensure we are exporting the right ARM_SUBSCRIPTION_ID when authenticating against workload zones.
+		if [[ "$ARM_SUBSCRIPTION_ID" != "$subscriptionId" ]]; then
+			ARM_SUBSCRIPTION_ID=$subscriptionId
+			export ARM_SUBSCRIPTION_ID
+		fi
+	fi
+
+}
