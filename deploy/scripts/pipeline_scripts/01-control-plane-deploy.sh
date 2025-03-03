c174c4a0dda76ffbd538a00a232f343e3646ae90
Kimmo Forss <kimforss@microsoft.com>
2025-03-03 23:29:58 +0200

    Simplify control plane deployment script conditional check for application configuration

--- deploy/scripts/pipeline_scripts/01-control-plane-deploy.sh
+++ deploy/scripts/pipeline_scripts/01-control-plane-deploy.sh
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-if printenv APPLICATION_CONFIGURATION_ID | grep -q "aks"; then
+if printenv APPLICATION_CONFIGURATION_ID; then
 	v2/01-control-plane-deploy.sh "$@"
 else
 	v1/01-control-plane-deploy.sh "$@"
