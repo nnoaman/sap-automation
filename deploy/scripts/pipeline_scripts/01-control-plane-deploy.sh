03fe7bf6550f972a94c5da48094956e27530b16f
Kimmo Forss <kimforss@microsoft.com>
2025-03-03 23:36:03 +0200

    Refactor control plane deployment scripts to use dynamic script paths for improved maintainability

--- deploy/scripts/pipeline_scripts/01-control-plane-deploy.sh
+++ deploy/scripts/pipeline_scripts/01-control-plane-deploy.sh
@@ -2,8 +2,11 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-if printenv APPLICATION_CONFIGURATION_ID; then
-	v2/01-control-plane-deploy.sh "$@"
+full_script_path="$(realpath "${BASH_SOURCE[0]}")"
+script_directory="$(dirname "${full_script_path}")"
+
+if printenv APPLICATION_CONFIGURATION_ID ; then
+	"${script_directory}/v2/01-control-plane-deploy.sh" "$@"
 else
-	v1/01-control-plane-deploy.sh "$@"
+	"${script_directory}/v1/01-control-plane-deploy.sh" "$@"
 fi
--- deploy/scripts/pipeline_scripts/01-control-plane-prepare.sh
+++ deploy/scripts/pipeline_scripts/01-control-plane-prepare.sh
@@ -2,8 +2,11 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+full_script_path="$(realpath "${BASH_SOURCE[0]}")"
+script_directory="$(dirname "${full_script_path}")"
+
 if printenv APPLICATION_CONFIGURATION_ID ; then
-	v2/01-control-plane-prepare.sh "$@"
+	"${script_directory}/v2/01-control-plane-prepare.sh" "$@"
 else
-	v1/01-control-plane-prepare.sh "$@"
+	"${script_directory}/v1/01-control-plane-prepare.sh" "$@"
 fi
