884e8ff8061102ce1e13439904ba2b702a45024a
Kimmo Forss <kimforss@microsoft.com>
2025-03-04 00:38:51 +0200

    Update SAP workload zone script to source deploy_utils.sh for enhanced functionality

--- deploy/scripts/pipeline_scripts/v2/02-sap-workload-zone.sh
+++ deploy/scripts/pipeline_scripts/v2/02-sap-workload-zone.sh
@@ -12,10 +12,11 @@ cyan="\e[1;36m"
 full_script_path="$(realpath "${BASH_SOURCE[0]}")"
 script_directory="$(dirname "${full_script_path}")"
 parent_directory="$(dirname "$script_directory")"
+grand_parent_directory="$(dirname "$parent_directory")"
 
 #call stack has full scriptname when using source
 source "${parent_directory}/helper.sh"
-DEBUG=False
+source "${grand_parent_directory}/deploy_utils.sh"
 
 DEBUG=False
 
