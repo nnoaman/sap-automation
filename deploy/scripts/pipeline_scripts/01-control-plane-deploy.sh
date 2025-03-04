9d016a7a20a7d06e5354b2f993a4bd995d946f6c
Kimmo Forss <kimforss@microsoft.com>
2025-03-04 09:56:07 +0200

    Add source command for deploy_utils.sh in control plane scripts for improved modularity

--- deploy/scripts/pipeline_scripts/01-control-plane-deploy.sh
+++ deploy/scripts/pipeline_scripts/01-control-plane-deploy.sh
@@ -4,6 +4,9 @@
 
 full_script_path="$(realpath "${BASH_SOURCE[0]}")"
 script_directory="$(dirname "${full_script_path}")"
+parent_directory="$(dirname "$script_directory")"
+
+source "${parent_directory}/deploy_utils.sh"
 
 if printenv APPLICATION_CONFIGURATION_ID; then
 	if is_valid_id "$APPLICATION_CONFIGURATION_ID" "/providers/Microsoft.AppConfiguration/configurationStores/"; then
