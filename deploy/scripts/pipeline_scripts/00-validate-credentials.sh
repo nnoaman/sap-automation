dd634e3c08a3bbca6f33e45453f203a9dc2b26b0
Kimmo Forss <kimforss@microsoft.com>
2025-03-10 22:33:55 +0200

    Refactor credential validation script to use Azure CLI for variable retrieval and remove unused environment variables

--- deploy/scripts/pipeline_scripts/00-validate-credentials.sh
+++ deploy/scripts/pipeline_scripts/00-validate-credentials.sh
@@ -2,60 +2,30 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-printenv | sort
+VARIABLE_GROUP_ID=$(az pipelines variable-group list --query "[?name=='$VARIABLE_GROUP'].id | [0]")
+echo '$VARIABLE_GROUP id: ' $VARIABLE_GROUP_ID
 
-# Description: This script validates the credentials and sets the output variables for the pipeline.
-if [ "${CHECK_ARM_SUBSCRIPTION_ID:0:2}" != '$(' ]; then
-	echo "##vso[task.setvariable variable=ARM_SUBSCRIPTION_ID;isOutput=true]$CHECK_ARM_SUBSCRIPTION_ID"
-else
-	test=$(printenv ARM_SUBSCRIPTION_ID)
-	if [ -n "$test" ]; then
-		echo "##vso[task.setvariable variable=ARM_SUBSCRIPTION_ID;isOutput=true]$ARM_SUBSCRIPTION_ID"
-	else
-		echo "##vso[task.setvariable variable=ARM_SUBSCRIPTION_ID;isOutput=true]"
-	fi
+az_var=$(az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --query "ARM_SUBSCRIPTION_ID.value")
+if [ -n "${az_var}" ]; then
+	echo "##vso[task.setvariable variable=ARM_SUBSCRIPTION_ID;isOutput=true]$az_var"
 fi
 
-if [ "${CHECK_ARM_CLIENT_ID:0:2}" != '$(' ]; then
-	echo "##vso[task.setvariable variable=ARM_SUBSCRIPTION_ID;isOutput=true]$CHECK_ARM_CLIENT_ID"
-else
-	test=$(printenv ARM_CLIENT_ID)
-	if [ -n "$test" ]; then
-		echo "##vso[task.setvariable variable=ARM_CLIENT_ID;isOutput=true]$ARM_CLIENT_ID"
-	else
-		echo "##vso[task.setvariable variable=ARM_CLIENT_ID;isOutput=true]"
-	fi
+az_var=$(az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --query "ARM_CLIENT_ID.value")
+if [ -n "${az_var}" ]; then
+	echo "##vso[task.setvariable variable=ARM_CLIENT_ID;isOutput=true]$az_var"
 fi
 
-if [ "${CHECK_ARM_CLIENT_SECRET:0:2}" != '$(' ]; then
-	echo "##vso[task.setvariable variable=ARM_CLIENT_SECRET;isOutput=true]$CHECK_ARM_CLIENT_SECRET"
-else
-	test=$(printenv ARM_CLIENT_SECRET)
-	if [ -n "$test" ]; then
-		echo "##vso[task.setvariable variable=ARM_CLIENT_SECRET;isOutput=true]$ARM_CLIENT_SECRET"
-	else
-		echo "##vso[task.setvariable variable=ARM_CLIENT_SECRET;isOutput=true]"
-	fi
+az_var=$(az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --query "ARM_CLIENT_SECRET.value")
+if [ -n "${az_var}" ]; then
+	echo "##vso[task.setvariable variable=ARM_CLIENT_SECRET;isOutput=true]$az_var"
 fi
 
-if [ "${CHECK_ARM_TENANT_ID:0:2}" != '$(' ]; then
-	echo "##vso[task.setvariable variable=ARM_TENANT_ID;isOutput=true]$CHECK_ARM_TENANT_ID"
-else
-	test=$(printenv ARM_TENANT_ID)
-	if [ -n "$test" ]; then
-		echo "##vso[task.setvariable variable=ARM_TENANT_ID;isOutput=true]$ARM_TENANT_ID"
-	else
-		echo "##vso[task.setvariable variable=ARM_TENANT_ID;isOutput=true]"
-	fi
+az_var=$(az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --query "ARM_TENANT_ID.value")
+if [ -n "${az_var}" ]; then
+	echo "##vso[task.setvariable variable=ARM_TENANT_ID;isOutput=true]$az_var"
 fi
 
-if [ "${CHECK_ARM_OBJECT_ID:0:2}" != '$(' ]; then
-	echo "##vso[task.setvariable variable=ARM_OBJECT_ID;isOutput=true]$CHECK_ARM_OBJECT_ID"
-else
-	test=$(printenv ARM_OBJECT_ID)
-	if [ -n "$test" ]; then
-		echo "##vso[task.setvariable variable=ARM_OBJECT_ID;isOutput=true]$ARM_OBJECT_ID"
-	else
-		echo "##vso[task.setvariable variable=ARM_OBJECT_ID;isOutput=true]"
-	fi
+az_var=$(az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --query "ARM_OBJECT_ID.value")
+if [ -n "${az_var}" ]; then
+	echo "##vso[task.setvariable variable=ARM_OBJECT_ID;isOutput=true]$az_var"
 fi
