a611521a617ab4f5c6bc1eb7b55a6a60b47cdd6f
Kimmo Forss <kimforss@microsoft.com>
2025-03-11 01:36:07 +0200

    Enhance credential validation script to fallback on environment variables for ARM credentials

--- deploy/scripts/pipeline_scripts/00-validate-credentials.sh
+++ deploy/scripts/pipeline_scripts/00-validate-credentials.sh
@@ -18,25 +18,45 @@ if [ -n "${VARIABLE_GROUP_ID}" ]; then
 	az_var=$(az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --query "ARM_SUBSCRIPTION_ID.value")
 	if [ -n "${az_var}" ]; then
 		echo "##vso[task.setvariable variable=ARM_SUBSCRIPTION_ID;isOutput=true]$az_var"
+	else
+		if printenv ARM_SUBSCRIPTION_ID; then
+			echo "##vso[task.setvariable variable=ARM_SUBSCRIPTION_ID;isOutput=true]$ARM_SUBSCRIPTION_ID"
+		fi
 	fi
 
 	az_var=$(az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --query "ARM_CLIENT_ID.value")
 	if [ -n "${az_var}" ]; then
 		echo "##vso[task.setvariable variable=ARM_CLIENT_ID;isOutput=true]$az_var"
+	else
+		if printenv ARM_CLIENT_ID; then
+			echo "##vso[task.setvariable variable=ARM_SUBSCRIPTION_ID;isOutput=true]$ARM_CLIENT_ID"
+		fi
 	fi
 
 	az_var=$(az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --query "ARM_CLIENT_SECRET.value")
 	if [ -n "${az_var}" ]; then
 		echo "##vso[task.setvariable variable=ARM_CLIENT_SECRET;isOutput=true;issecret=true]$az_var"
+	else
+		if printenv ARM_CLIENT_SECRET; then
+			echo "##vso[task.setvariable variable=ARM_SUBSCRIPTION_ID;isOutput=true]$ARM_CLIENT_SECRET"
+		fi
 	fi
 
 	az_var=$(az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --query "ARM_TENANT_ID.value")
 	if [ -n "${az_var}" ]; then
 		echo "##vso[task.setvariable variable=ARM_TENANT_ID;isOutput=true]$az_var"
+	else
+		if printenv ARM_TENANT_ID; then
+			echo "##vso[task.setvariable variable=ARM_SUBSCRIPTION_ID;isOutput=true]$ARM_TENANT_ID"
+		fi
 	fi
 
 	az_var=$(az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --query "ARM_OBJECT_ID.value")
 	if [ -n "${az_var}" ]; then
 		echo "##vso[task.setvariable variable=ARM_OBJECT_ID;isOutput=true]$az_var"
+	else
+		if printenv ARM_OBJECT_ID; then
+			echo "##vso[task.setvariable variable=ARM_SUBSCRIPTION_ID;isOutput=true]$ARM_OBJECT_ID"
+		fi
 	fi
 fi
