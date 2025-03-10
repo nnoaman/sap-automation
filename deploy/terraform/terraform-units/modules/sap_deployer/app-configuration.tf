c34e2fcb62095f662e0a2912c6afd018de88e167
Kimmo Forss <kimforss@microsoft.com>
2025-03-10 19:08:04 +0200

    Add missing dependency for time_sleep resource in app-configuration.tf

--- deploy/terraform/terraform-units/modules/sap_deployer/app-configuration.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/app-configuration.tf
@@ -49,7 +49,7 @@ resource "time_sleep" "wait_for_appconf_dataowner_assignment" {
   create_duration                      = "60s"
 
   depends_on                           = [
-                                           azurerm_role_assignment.appconf_dataowner_msi
+                                           azurerm_role_assignment.appconf_dataowner_msi,
                                            azurerm_role_assignment.appconf_dataowner
                                          ]
 }
