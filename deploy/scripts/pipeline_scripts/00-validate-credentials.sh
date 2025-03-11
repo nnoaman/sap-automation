8f04ba712b7f70ce01d55e414058458b5a1f2999
Kimmo Forss <kimforss@microsoft.com>
2025-03-11 13:17:35 +0200

    Update variable references in SAP deployment scripts for improved clarity and validation

--- deploy/scripts/pipeline_scripts/00-validate-credentials.sh
+++ deploy/scripts/pipeline_scripts/00-validate-credentials.sh
@@ -12,6 +12,24 @@ VARIABLE_GROUP_ID=$(az pipelines variable-group list --query "[?name=='$VARIABLE
 if [ -n "${VARIABLE_GROUP_ID}" ]; then
 	echo '$VARIABLE_GROUP id: ' $VARIABLE_GROUP_ID
 
+	az_var=$(az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --query "APPLICATION_CONFIGURATION_ID.value")
+	if [ -n "${az_var}" ]; then
+		echo "##vso[task.setvariable variable=APPLICATION_CONFIGURATION_ID;isOutput=true]$az_var"
+	else
+		if printenv APPLICATION_CONFIGURATION_ID; then
+			echo "##vso[task.setvariable variable=APPLICATION_CONFIGURATION_ID;isOutput=true]$APPLICATION_CONFIGURATION_ID"
+		fi
+	fi
+
+	az_var=$(az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --query "CONTROL_PLANE_NAME.value")
+	if [ -n "${az_var}" ]; then
+		echo "##vso[task.setvariable variable=CONTROL_PLANE_NAME;isOutput=true]$az_var"
+	else
+		if printenv CONTROL_PLANE_NAME; then
+			echo "##vso[task.setvariable variable=CONTROL_PLANE_NAME;isOutput=true]$CONTROL_PLANE_NAME"
+		fi
+	fi
+
 	az_var=$(az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --query "ARM_SUBSCRIPTION_ID.value")
 	if [ -n "${az_var}" ]; then
 		echo "##vso[task.setvariable variable=ARM_SUBSCRIPTION_ID;isOutput=true]$az_var"
