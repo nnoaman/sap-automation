1d4a50b157da942264b0c5f3ba77dee641dd596a
Kimmo Forss <kimforss@microsoft.com>
2025-03-13 14:28:55 +0200

    Refactor pipeline scripts to use dynamic script names for improved maintainability

--- deploy/scripts/pipeline_scripts/10-remover-terraform-workload-zone.sh
+++ deploy/scripts/pipeline_scripts/10-remover-terraform-workload-zone.sh
@@ -5,18 +5,20 @@
 full_script_path="$(realpath "${BASH_SOURCE[0]}")"
 script_directory="$(dirname "${full_script_path}")"
 parent_directory="$(dirname "$script_directory")"
+SCRIPT_NAME="$(basename "$0")"
+readonly SCRIPT_NAME
 
 source "${parent_directory}/deploy_utils.sh"
 
 if printenv APPLICATION_CONFIGURATION_ID; then
 	if is_valid_id "$APPLICATION_CONFIGURATION_ID" "/providers/Microsoft.AppConfiguration/configurationStores/"; then
 		echo "Running v2 script"
-		"${script_directory}/v2/10-remover-terraform-workload-zone.sh" "$@"
+		"${script_directory}/v2/$SCRIPT_NAME" "$@"
 	else
 		echo "Running v1 script"
-		"${script_directory}/v1/10-remover-terraform-workload-zone.sh" "$@"
+		"${script_directory}/v1/$SCRIPT_NAME" "$@"
 	fi
 else
 	echo "Running v1 script"
-	"${script_directory}/v1/10-remover-terraform-workload-zone.sh" "$@"
+	"${script_directory}/v1/$SCRIPT_NAME" "$@"
 fi
