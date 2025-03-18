be3c3210a92faea2b3afcdfa54c35c9ae5edbec1
Kimmo Forss <kimforss@microsoft.com>
2025-03-19 00:27:02 +0200

    Enhance validation logic for Key Vault ID variables to check for non-empty values before length validation

--- deploy/terraform/terraform-units/modules/sap_landscape/variables_global.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/variables_global.tf
@@ -89,18 +89,18 @@ variable "key_vault"                                    {
                                                           validation {
                                                                        condition = (
                                                                          contains(keys(var.key_vault), "keyvault_id_for_deployment_credentials") ? (
-                                                                           length(split("/", var.key_vault.keyvault_id_for_deployment_credentials)) == 9) : (
+                                                                           length(var.key_vault.keyvault_id_for_deployment_credentials) >0 ? length(split("/", var.key_vault.keyvault_id_for_deployment_credentials)) == 9) : (
                                                                            true
-                                                                         )
+                                                                         ) : true
                                                                        )
                                                                        error_message = "If specified, the spn_keyvault_id needs to be a correctly formed Azure resource ID."
                                                                      }
                                                           validation {
                                                                        condition = (
                                                                          contains(keys(var.key_vault), "keyvault_id_for_system_credentials") ? (
-                                                                           length(split("/", var.key_vault.keyvault_id_for_system_credentials)) == 9) : (
+                                                                           length(var.key_vault.keyvault_id_for_system_credentials) > 0 ? length(split("/", var.key_vault.keyvault_id_for_system_credentials)) == 9) : (
                                                                            true
-                                                                         )
+                                                                         ) : true
                                                                        )
                                                                        error_message = "If specified, the user_keyvault_id needs to be a correctly formed Azure resource ID."
                                                                      }
