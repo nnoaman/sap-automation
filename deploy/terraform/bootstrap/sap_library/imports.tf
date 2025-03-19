7b1f2e5ebb65f791b47b52bec1abfd192d2d1807
Kimmo Forss <kimforss@microsoft.com>
2025-03-19 23:56:02 +0200

    Refactor ephemeral Key Vault secret configurations for client_id and tenant_id in sap_library module

--- deploy/terraform/bootstrap/sap_library/imports.tf
+++ deploy/terraform/bootstrap/sap_library/imports.tf
@@ -24,7 +24,7 @@ data "azurerm_key_vault_secret" "subscription_id" {
                                                   }
 
 
-ephemeral "azurerm_key_vault_secret" "client_id"       {
+ephemeral "azurerm_key_vault_secret" "client_id"  {
                                                     count        = length(local.key_vault.kv_spn_id) > 0 ? (var.use_deployer && var.use_spn ? 1 : 0) : 0
                                                     name         = format("%s-client-id", upper(data.terraform_remote_state.deployer[0].outputs.control_plane_name))
                                                     key_vault_id = local.key_vault.kv_spn_id
@@ -36,13 +36,9 @@ ephemeral "azurerm_key_vault_secret" "client_secret"   {
                                                     key_vault_id = local.key_vault.kv_spn_id
                                                   }
 
-ephemeral "azurerm_key_vault_secret" "tenant_id"       {
+ephemeral "azurerm_key_vault_secret" "tenant_id"  {
                                                     count        = length(local.key_vault.kv_spn_id) > 0 ? (var.use_deployer && var.use_spn ? 1 : 0) : 0
                                                     name         = format("%s-tenant-id", upper(data.terraform_remote_state.deployer[0].outputs.control_plane_name))
                                                     key_vault_id = local.key_vault.kv_spn_id
                                                   }
 // Import current service principal
-data "azuread_service_principal" "sp"             {
-                                                    count        = local.use_spn ? 1 : 0
-                                                    client_id    = local.spn.client_id
-                                                  }
