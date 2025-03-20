4a478601cd3bab24100c139ddb905756c7a6e210
Kimmo Forss <kimforss@microsoft.com>
2025-03-20 10:43:09 +0200

    Refactor Azure provider configuration to use key vault secrets for subscription and service principal details

--- deploy/terraform/run/sap_library/imports.tf
+++ deploy/terraform/run/sap_library/imports.tf
@@ -46,9 +46,3 @@ ephemeral "azurerm_key_vault_secret" "tenant_id"       {
                                                     name         = format("%s-tenant-id", upper(data.terraform_remote_state.deployer[0].outputs.control_plane_name))
                                                     key_vault_id = local.key_vault.kv_spn_id
                                                   }
-
-// Import current service principal
-data "azuread_service_principal" "sp"             {
-                                                    count          = local.use_spn ? 1 : 0
-                                                    client_id      = local.spn.client_id
-                                                  }
--- deploy/terraform/run/sap_library/providers.tf
+++ deploy/terraform/run/sap_library/providers.tf
@@ -39,10 +39,10 @@ provider "azurerm"                     {
                                                                    }
 
                                                   }
-                                         subscription_id            = local.spn.subscription_id
-                                         client_id                  = local.use_spn ? local.spn.client_id : null
-                                         client_secret              = local.use_spn ? local.spn.client_secret : null
-                                         tenant_id                  = local.use_spn ? local.spn.tenant_id : null
+                                         subscription_id            = try(data.azurerm_key_vault_secret.subscription_id[0].value, null)
+                                         client_id                  = try(data.azurerm_key_vault_secret.client_id[0].value, null)
+                                         client_secret              = try(ephemeral.azurerm_key_vault_secret.client_secret[0].value, null)
+                                         tenant_id                  = try(ephemeral.azurerm_key_vault_secret.tenant_id[0].value, null)
                                          partner_id                 = "140c3bc9-c937-4139-874f-88288bab08bb"
                                          storage_use_azuread        = !var.shared_access_key_enabled
                                          use_msi                    = var.use_spn ? false : true
@@ -63,7 +63,7 @@ provider "azurerm"                     {
                                          features {
                                                   }
                                          alias                      = "dnsmanagement"
-                                         subscription_id            = try(coalesce(var.management_dns_subscription_id, local.spn.subscription_id), null)
+                                         subscription_id            = try(coalesce(var.management_dns_subscription_id, data.azurerm_key_vault_secret.subscription_id[0].value), null)
                                          client_id                  = try(data.azurerm_key_vault_secret.client_id[0].value, null)
                                          client_secret              = try(ephemeral.azurerm_key_vault_secret.client_secret[0].value, null)
                                          tenant_id                  = try(ephemeral.azurerm_key_vault_secret.tenant_id[0].value, null)
@@ -73,7 +73,7 @@ provider "azurerm"                     {
 
 provider "azurerm"                     {
                                          features {}
-                                         subscription_id            = try(coalesce(var.privatelink_dns_subscription_id, local.spn.subscription_id), null)
+                                         subscription_id            = try(coalesce(var.management_dns_subscription_id, data.azurerm_key_vault_secret.subscription_id[0].value), null)
                                          client_id                  = try(data.azurerm_key_vault_secret.client_id[0].value, null)
                                          client_secret              = try(ephemeral.azurerm_key_vault_secret.client_secret[0].value, null)
                                          tenant_id                  = try(ephemeral.azurerm_key_vault_secret.tenant_id[0].value, null)
