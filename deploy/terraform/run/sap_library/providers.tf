03da61f5ac9854c7c8b30e43c79ac2b0cfcae89b
Kimmo Forss <kimforss@microsoft.com>
2025-03-20 13:41:34 +0200

    Refactor Azure provider configuration to retrieve client ID, client secret, and tenant ID from Key Vault for improved security and consistency

--- deploy/terraform/run/sap_library/providers.tf
+++ deploy/terraform/run/sap_library/providers.tf
@@ -49,9 +49,9 @@ provider "azurerm"                     {
                                                   }
 
                                          subscription_id            = var.subscription_id
-                                         client_id                  = local.use_spn ? local.spn.client_id : null
-                                         client_secret              = local.use_spn ? try(ephemeral.azurerm_key_vault_secret.client_secret[0].value, null) : null
-                                         tenant_id                  = local.use_spn ? local.spn.tenant_id : null
+                                         client_id                  = try(data.azurerm_key_vault_secret.client_id[0].value, null)
+                                         client_secret              = try(ephemeral.azurerm_key_vault_secret.client_secret[0].value, null)
+                                         tenant_id                  = try(ephemeral.azurerm_key_vault_secret.tenant_id[0].value, null)
 
                                          alias                      = "main"
 
@@ -73,10 +73,10 @@ provider "azurerm"                     {
 provider "azurerm"                     {
                                          features {}
                                          subscription_id            = try(coalesce(var.management_dns_subscription_id, var.subscription_id), null)
-                                         client_id                  = local.use_spn ? local.spn.client_id : null
-                                         client_secret              = local.use_spn ? local.spn.client_secret : null
-                                         tenant_id                  = local.use_spn ? local.spn.tenant_id : null
                                          alias                      = "dnsmanagement"
+                                         client_id                  = try(data.azurerm_key_vault_secret.client_id[0].value, null)
+                                         client_secret              = try(ephemeral.azurerm_key_vault_secret.client_secret[0].value, null)
+                                         tenant_id                  = try(ephemeral.azurerm_key_vault_secret.tenant_id[0].value, null)
 
                                          storage_use_azuread        = true
                                          use_msi                    = var.use_spn ? false : true
@@ -85,9 +85,9 @@ provider "azurerm"                     {
 provider "azurerm"                     {
                                          features {}
                                          subscription_id            = try(coalesce(var.privatelink_dns_subscription_id, var.subscription_id), null)
-                                         client_id                  = local.use_spn ? local.spn.client_id : null
-                                         client_secret              = local.use_spn ? try(ephemeral.azurerm_key_vault_secret.client_secret[0].value, null) : null
-                                         tenant_id                  = local.use_spn ? local.spn.tenant_id : null
+                                         client_id                  = try(data.azurerm_key_vault_secret.client_id[0].value, null)
+                                         client_secret              = try(ephemeral.azurerm_key_vault_secret.client_secret[0].value, null)
+                                         tenant_id                  = try(ephemeral.azurerm_key_vault_secret.tenant_id[0].value, null)
                                          alias                      = "privatelinkdnsmanagement"
 
                                          storage_use_azuread        = true
@@ -97,7 +97,7 @@ provider "azurerm"                     {
 provider "azuread"                     {
                                          client_id                  = local.spn.client_id
                                          client_secret              = try(ephemeral.azurerm_key_vault_secret.client_secret[0].value, null)
-                                         tenant_id                  = local.spn.tenant_id
+                                         tenant_id                  = try(ephemeral.azurerm_key_vault_secret.tenant_id[0].value, null)
                                        }
 
 terraform                              {
