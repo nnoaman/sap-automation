58acb0db8382d69e9de89e0a1de6c383d07f7340
Kimmo Forss <kimforss@microsoft.com>
2025-03-20 14:57:26 +0200

    Refactor Azure provider configuration to remove sensitive client credentials and enable managed identity support for improved security

--- deploy/terraform/run/sap_library/providers.tf
+++ deploy/terraform/run/sap_library/providers.tf
@@ -74,24 +74,26 @@ provider "azurerm"                     {
                                          features {}
                                          subscription_id            = try(coalesce(var.management_dns_subscription_id, var.subscription_id), null)
                                          alias                      = "dnsmanagement"
-                                         client_id                  = try(data.azurerm_key_vault_secret.client_id[0].value, null)
-                                         client_secret              = try(ephemeral.azurerm_key_vault_secret.client_secret[0].value, null)
-                                         tenant_id                  = try(data.azurerm_key_vault_secret.tenant_id[0].value, null)
+                                         #client_id                  = try(data.azurerm_key_vault_secret.client_id[0].value, null)
+                                         #client_secret              = try(ephemeral.azurerm_key_vault_secret.client_secret[0].value, null)
+                                         #tenant_id                  = try(data.azurerm_key_vault_secret.tenant_id[0].value, null)
 
                                          storage_use_azuread        = true
-                                         use_msi                    = var.use_spn ? false : true
+                                         #use_msi                    = var.use_spn ? false : true
+                                         use_msi                    = true
                                        }
 
 provider "azurerm"                     {
                                          features {}
                                          subscription_id            = try(coalesce(var.privatelink_dns_subscription_id, var.subscription_id), null)
-                                         client_id                  = try(data.azurerm_key_vault_secret.client_id[0].value, null)
-                                         client_secret              = try(ephemeral.azurerm_key_vault_secret.client_secret[0].value, null)
-                                         tenant_id                  = try(data.azurerm_key_vault_secret.tenant_id[0].value, null)
+                                         #client_id                  = try(data.azurerm_key_vault_secret.client_id[0].value, null)
+                                         #client_secret              = try(ephemeral.azurerm_key_vault_secret.client_secret[0].value, null)
+                                         #tenant_id                  = try(data.azurerm_key_vault_secret.tenant_id[0].value, null)
                                          alias                      = "privatelinkdnsmanagement"
 
                                          storage_use_azuread        = true
-                                         use_msi                    = var.use_spn ? false : true
+                                         #use_msi                    = var.use_spn ? false : true
+                                         use_msi                    = true
                                        }
 
 provider "azuread"                     {
