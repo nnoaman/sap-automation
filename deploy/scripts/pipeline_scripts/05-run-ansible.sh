40e2a39d245cdd217c1e44fc0ee47d86534af981
hdamecharla <hdamecharla@microsoft.com>
2025-03-21 08:42:51 +0530

    Refactor ansible script in 05-run-ansible.sh; improve quoting and variable handling for better readability and reliability

--- deploy/scripts/pipeline_scripts/05-run-ansible.sh
+++ deploy/scripts/pipeline_scripts/05-run-ansible.sh
@@ -51,18 +51,18 @@ export control_plane_subscription
 
 vault_name=$(echo "${VAULT_NAME}" | tr [:upper:] [:lower:] | xargs)
 
-az account set --subscription $AZURE_SUBSCRIPTION_ID --output none
+az account set --subscription "$AZURE_SUBSCRIPTION_ID" --output none
 
 set -eu
 
-if [ ! -f $PARAMETERS_FOLDER/sshkey ]; then
+if [ ! -f "$PARAMETERS_FOLDER"/sshkey ]; then
 	echo "##[section]Retrieving sshkey..."
 	az keyvault secret show --name "$SSH_KEY_NAME" --vault-name "$vault_name" --subscription "$control_plane_subscription" --query value --output tsv >"$PARAMETERS_FOLDER/sshkey"
 	sudo chmod 600 "$PARAMETERS_FOLDER"/sshkey
 fi
 
 password_secret=$(az keyvault secret show --name "$PASSWORD_KEY_NAME" --vault-name "$vault_name" --query value --output tsv)
-ANSIBLE_PASSWORD=$password_secret
+ANSIBLE_PASSWORD="${password_secret}"
 export ANSIBLE_PASSWORD
 
 echo "Extra parameters passed: " "$EXTRA_PARAMS"
@@ -72,16 +72,16 @@ base=$(basename "$ANSIBLE_FILE_PATH")
 filename_without_prefix=$(echo "$base" | awk -F'.' '{print $1}')
 return_code=0
 
-echo "Extra parameters passed: " $EXTRA_PARAMS
+echo "Extra parameters passed: $EXTRA_PARAMS"
 echo "Check for file: ${filename_without_prefix}"
 
 command="ansible --version"
-eval $command
+eval "${command}"
 
 EXTRA_PARAM_FILE=""
 
-if [ -f $PARAMETERS_FOLDER/extra-params.yaml ]; then
-	echo "Extra parameter file passed: " $PARAMETERS_FOLDER/extra-params.yaml
+if [ -f "$PARAMETERS_FOLDER"/extra-params.yaml ]; then
+	echo "Extra parameter file passed: $PARAMETERS_FOLDER/extra-params.yaml"
 
 	EXTRA_PARAM_FILE="-e @$PARAMETERS_FOLDER/extra-params.yaml"
 fi
@@ -96,30 +96,36 @@ filename=./config/Ansible/"${filename_without_prefix}"_pre.yml
 if [ -f "${filename}" ]; then
 	echo "##[group]- preconfiguration"
 
-	redacted_command="ansible-playbook -i $INVENTORY -e @$SAP_PARAMS "$EXTRA_PARAMS" $EXTRA_PARAM_FILE ${filename} -e 'kv_name=$vault_name'"
+	redacted_command="ansible-playbook -i $INVENTORY -e @$SAP_PARAMS '$EXTRA_PARAMS' \
+										$EXTRA_PARAM_FILE ${filename} -e 'kv_name=$vault_name'"
 	echo "##[section]Executing [$redacted_command]..."
 
-	command="ansible-playbook -i $INVENTORY --private-key $PARAMETERS_FOLDER/sshkey  -e 'kv_name=$vault_name' \
-            -e @$SAP_PARAMS -e 'download_directory=$AGENT_TEMPDIRECTORY' -e '_workspace_directory=$PARAMETERS_FOLDER' "$EXTRA_PARAMS"  \
-            -e ansible_ssh_pass='{{ lookup('env', 'ANSIBLE_PASSWORD') }}' $EXTRA_PARAM_FILE ${filename}"
+	command="ansible-playbook -i $INVENTORY --private-key $PARAMETERS_FOLDER/sshkey    \
+						-e 'kv_name=$vault_name' -e @$SAP_PARAMS                                 \
+						-e 'download_directory=$AGENT_TEMPDIRECTORY'                             \
+						-e '_workspace_directory=$PARAMETERS_FOLDER' '$EXTRA_PARAMS'             \
+            -e ansible_ssh_pass='${ANSIBLE_PASSWORD}' $EXTRA_PARAM_FILE ${filename}"
 
-	eval $command
+	eval "${command}"
 	return_code=$?
 	echo "##[section]Ansible playbook ${filename} execution completed with exit code [$return_code]"
 	echo "##[endgroup]"
 
 fi
 
-command="ansible-playbook -i $INVENTORY --private-key $PARAMETERS_FOLDER/sshkey   -e 'kv_name=$vault_name'   \
-      -e @$SAP_PARAMS -e 'download_directory=$AGENT_TEMPDIRECTORY' -e '_workspace_directory=$PARAMETERS_FOLDER' \
-      -e ansible_ssh_pass='{{ lookup('env', 'ANSIBLE_PASSWORD') }}' "$EXTRA_PARAMS" $EXTRA_PARAM_FILE                                  \
-       $ANSIBLE_FILE_PATH"
+command="ansible-playbook -i $INVENTORY --private-key $PARAMETERS_FOLDER/sshkey       \
+					-e 'kv_name=$vault_name' -e @$SAP_PARAMS                                    \
+					-e 'download_directory=$AGENT_TEMPDIRECTORY'                                \
+					-e '_workspace_directory=$PARAMETERS_FOLDER'                                \
+					-e ansible_ssh_pass='${ANSIBLE_PASSWORD}' '$EXTRA_PARAMS' $EXTRA_PARAM_FILE \
+          $ANSIBLE_FILE_PATH"
 
-redacted_command="ansible-playbook -i $INVENTORY -e @$SAP_PARAMS "$EXTRA_PARAMS" $EXTRA_PARAM_FILE $ANSIBLE_FILE_PATH  -e 'kv_name=$vault_name'"
+redacted_command="ansible-playbook -i $INVENTORY -e @$SAP_PARAMS '$EXTRA_PARAMS'      \
+									$EXTRA_PARAM_FILE $ANSIBLE_FILE_PATH  -e 'kv_name=$vault_name'"
 
 echo "##[section]Executing [$redacted_command]..."
 echo "##[group]- output"
-eval $command
+eval "${command}"
 return_code=$?
 echo "##[section]Ansible playbook execution completed with exit code [$return_code]"
 echo "##[endgroup]"
@@ -133,17 +139,21 @@ echo "##[endgroup]"
 filename=./config/Ansible/"${filename_without_prefix}"_post.yml
 echo "Check for file: ${filename}"
 
-if [ -f ${filename} ]; then
+if [ -f "${filename}" ]; then
 
 	echo "##[group]- postconfiguration"
-	redacted_command="ansible-playbook -i "$INVENTORY" -e @"$SAP_PARAMS" "$EXTRA_PARAMS" $EXTRA_PARAM_FILE "${filename}"  -e 'kv_name=$vault_name'"
+	redacted_command="ansible-playbook -i '$INVENTORY' -e @'$SAP_PARAMS' '$EXTRA_PARAMS' \
+										'$EXTRA_PARAM_FILE' '${filename}'  -e 'kv_name=$vault_name'"
 	echo "##[section]Executing [$redacted_command]..."
 
-	command="ansible-playbook -i "$INVENTORY" --private-key $PARAMETERS_FOLDER/sshkey   -e 'kv_name=$vault_name'      \
-            -e @$SAP_PARAMS -e 'download_directory=$AGENT_TEMPDIRECTORY' -e '_workspace_directory=$PARAMETERS_FOLDER' \
-            -e ansible_ssh_pass='{{ lookup('env', 'ANSIBLE_PASSWORD') }}' ${filename}  "$EXTRA_PARAMS" $EXTRA_PARAM_FILE"
+	command="ansible-playbook -i '$INVENTORY' --private-key '$PARAMETERS_FOLDER/sshkey'   \
+						-e 'kv_name=$vault_name' -e @$SAP_PARAMS                                    \
+						-e 'download_directory=$AGENT_TEMPDIRECTORY'                                \
+						-e '_workspace_directory=$PARAMETERS_FOLDER'                                \
+						-e ansible_ssh_pass='${ANSIBLE_PASSWORD}' '${filename}' '$EXTRA_PARAMS'     \
+						$EXTRA_PARAM_FILE"
 
-	eval $command
+	eval "${command}"
 	return_code=$?
 	echo "##[section]Ansible playbook ${filename} execution completed with exit code [$return_code]"
 	echo "##[endgroup]"
