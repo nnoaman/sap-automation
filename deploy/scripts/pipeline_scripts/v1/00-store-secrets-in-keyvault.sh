85d9868931262b3f7e10b34de8ebc771657a4b00
Kimmo Forss <kimforss@microsoft.com>
2025-03-21 12:27:29 +0200

    Update banner title and improve error messages in key vault script; streamline variable handling and logging

--- deploy/scripts/pipeline_scripts/v1/00-store-secrets-in-keyvault.sh
+++ deploy/scripts/pipeline_scripts/v1/00-store-secrets-in-keyvault.sh
@@ -17,7 +17,7 @@ grand_parent_directory="$(dirname "$parent_directory")"
 SCRIPT_NAME="$(basename "$0")"
 readonly SCRIPT_NAME
 
-banner_title="Deploy Workload Zone"
+banner_title="Set Workload Zone Secrets"
 
 #call stack has full script name when using source
 # shellcheck disable=SC1091
@@ -40,9 +40,7 @@ set -eu
 
 print_banner "$banner_title" "Starting $SCRIPT_NAME" "info"
 
-WORKLOAD_ZONE_NAME=$(echo "$WORKLOAD_ZONE_FOLDERNAME" | cut -d'-' -f1-3)
-
-echo "##vso[build.updatebuildnumber]Setting the deployment credentials for the SAP Workload zone defined in $WORKLOAD_ZONE_FOLDERNAME"
+echo "##vso[build.updatebuildnumber]Setting the deployment credentials for the SAP Workload zone defined in $ZONE"
 
 echo -e "$green--- Checkout $BUILD_SOURCEBRANCHNAME ---$reset"
 
@@ -82,14 +80,11 @@ if [[ ! -f /etc/profile.d/deploy_server.sh ]]; then
 		echo "##vso[task.logissue type=error]az login failed."
 		exit 2
 	fi
-	else
-		ARM_USE_MSI=true
-		export ARM_USE_MSI
-		ARM_CLIENT_ID=$(grep -m 1 "export ARM_CLIENT_ID=" /etc/profile.d/deploy_server.sh | awk -F'=' '{print $2}' | xargs)
-		export ARM_CLIENT_ID
-
-		ARM_USE_MSI=$(grep -m 1 "export ARM_USE_MSI=" /etc/profile.d/deploy_server.sh | awk -F'=' '{print $2}' | xargs)
-		export ARM_USE_MSI
+else
+	ARM_USE_MSI=true
+	export ARM_USE_MSI
+	ARM_CLIENT_ID=$(grep -m 1 "export ARM_CLIENT_ID=" /etc/profile.d/deploy_server.sh | awk -F'=' '{print $2}' | xargs)
+	export ARM_CLIENT_ID
 
 fi
 
@@ -105,16 +100,39 @@ echo "Azure CLI version:"
 echo "-------------------------------------------------"
 az --version
 
-
 az devops configure --defaults organization=$SYSTEM_COLLECTIONURI project='$SYSTEM_TEAMPROJECT' --output none
 
 VARIABLE_GROUP_ID=$(az pipelines variable-group list --query "[?name=='$VARIABLE_GROUP'].id | [0]")
+PARENT_VARIABLE_GROUP_ID=$(az pipelines variable-group list --query "[?name=='$PARENT_VARIABLE_GROUP'].id | [0]")
 
 if [ -z "${VARIABLE_GROUP_ID}" ]; then
 	echo "##vso[task.logissue type=error]Variable group $VARIABLE_GROUP could not be found."
 	exit 2
 fi
+
+if [ -z "${PARENT_VARIABLE_GROUP_ID}" ]; then
+	echo "##vso[task.logissue type=error]Variable group $PARENT_VARIABLE_GROUP could not be found."
+	exit 2
+fi
+
+environment_file_name="$CONFIG_REPO_PATH/.sap_deployment_automation/${CONTROL_PLANE_NAME}"
+
+APPLICATION_CONFIGURATION_ID=$(az pipelines variable-group variable list --group-id "${PARENT_VARIABLE_GROUP_ID}" --query "APPLICATION_CONFIGURATION_ID.value" --output tsv)
+if is_valid_id "$APPLICATION_CONFIGURATION_ID" "/providers/Microsoft.AppConfiguration/configurationStores/"; then
+
+	key_vault_id=$(getVariableFromApplicationConfiguration "$APPLICATION_CONFIGURATION_ID" "${CONTROL_PLANE_NAME}_KeyVaultResourceId" "${CONTROL_PLANE_NAME}")
+	if [ -z "$key_vault_id" ]; then
+		echo "##vso[task.logissue type=warning]Key '${CONTROL_PLANE_NAME}_KeyVaultResourceId' was not found in the application configuration ( '$application_configuration_name' )."
+	fi
+else
+	load_config_vars "${environment_file_name}" "keyvault"
+	key_vault="$keyvault"
+	key_vault_id=$(az resource list --name "${keyvault}" --subscription "$ARM_SUBSCRIPTION_ID" --resource-type Microsoft.KeyVault/vaults --query "[].id | [0]" -o tsv)
+
+fi
+
 export VARIABLE_GROUP_ID
+export PARENT_VARIABLE_GROUP_ID
 
 printf -v tempval '%s id:' "$VARIABLE_GROUP"
 printf -v val '%-20s' "${tempval}"
@@ -122,25 +140,20 @@ echo "$val                 $VARIABLE_GROUP_ID"
 
 echo -e "$green--- Read parameter values ---$reset"
 
-dos2unix -q "${workload_environment_file_name}"
-
 deployer_tfstate_key=$CONTROL_PLANE_NAME.terraform.tfstate
 export deployer_tfstate_key
-
-load_config_vars "${workload_environment_file_name}" "keyvault"
-key_vault="$keyvault"
-key_vault_id=$(az resource list --name "${keyvault}" --subscription "$ARM_SUBSCRIPTION_ID" --resource-type Microsoft.KeyVault/vaults --query "[].id | [0]" -o tsv)
-
 
 keyvault_subscription_id=$(echo "$key_vault_id" | cut -d '/' -f 3)
 key_vault=$(echo "$key_vault_id" | cut -d '/' -f 9)
 
 if [ -z "$key_vault" ]; then
-	echo "##vso[task.logissue type=error]Key vault name (${CONTROL_PLANE_NAME}_KeyVaultName) was not found in the application configuration ( '$application_configuration_name' )."
+	echo "##vso[task.logissue type=error]Key vault name (${CONTROL_PLANE_NAME}_KeyVaultName) was not found in the application configuration ( '$application_configuration_name' ) or in configuration file ( ${environment_file_name} )."
+	print_banner "$banner_title" "Key vault name (${CONTROL_PLANE_NAME}_KeyVaultName) was not found in the application configuration ( '$application_configuration_name' ) or in configuration file ( ${environment_file_name}" "error"
 	exit 2
 fi
 
 if [ "$USE_MSI" != "true" ]; then
+
 	if "$SAP_AUTOMATION_REPO_PATH/deploy/scripts/set_secrets_v2.sh" --prefix "${CONTROL_PLANE_NAME}" --key_vault "${key_vault}" --keyvault_subscription "$keyvault_subscription_id" \
 		--subscription "$ARM_SUBSCRIPTION_ID" --client_id "$CLIENT_ID" --client_secret "$CLIENT_SECRET" --client_tenant_id "$TENANT_ID" --ado; then
 		return_code=$?
@@ -149,9 +162,17 @@ if [ "$USE_MSI" != "true" ]; then
 		print_banner "$banner_title - Set secrets" "Set_secrets failed" "error"
 		exit $return_code
 	fi
+else
+	if "$SAP_AUTOMATION_REPO_PATH/deploy/scripts/set_secrets_v2.sh" --prefix "${CONTROL_PLANE_NAME}" --key_vault "${key_vault}" --keyvault_subscription "$keyvault_subscription_id" \
+		--subscription "$ARM_SUBSCRIPTION_ID" --msi --ado; then
+		return_code=$?
+	else
+		return_code=$?
+		print_banner "$banner_title - Set secrets" "Set_secrets failed" "error"
+		exit $return_code
+	fi
+
 fi
-
-
 print_banner "$banner_title" "Exiting $SCRIPT_NAME" "info"
 
 exit $return_code
