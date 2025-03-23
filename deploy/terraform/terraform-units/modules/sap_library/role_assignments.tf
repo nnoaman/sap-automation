ea10b8c06cf85af9e8e92dc82510d9568066d6ed
Kimmo Forss <kimforss@microsoft.com>
2025-03-23 11:37:50 +0200

    Add ABAC

--- deploy/terraform/terraform-units/modules/sap_library/role_assignments.tf
+++ deploy/terraform/terraform-units/modules/sap_library/role_assignments.tf
@@ -68,3 +68,38 @@ resource "azurerm_role_assignment" "resource_group_contributor_spn" {
   principal_type                       = "ServicePrincipal"
   principal_id                         = var.infrastructure.spn_id
 }
+
+
+resource "azurerm_role_assignment" "resource_group_user_access_admin_spn" {
+  provider                             = azurerm.main
+  count                                = var.infrastructure.assign_permissions && length(var.infrastructure.spn_id) > 0 ? 1 : 0
+  scope                                = local.resource_group_exists ? (
+                                                 data.azurerm_resource_group.library[0].id) : (
+                                                 azurerm_resource_group.library[0].id
+                                               )
+  role_definition_name                 = "User Access Administrator"
+  principal_type                       = "ServicePrincipal"
+  principal_id                         = var.infrastructure.spn_id
+  condition_version                    = "2.0"
+  condition                            = <<-EOT
+                                            (
+                                             (
+                                              !(ActionMatches{'Microsoft.Authorization/roleAssignments/write'})
+                                             )
+                                             OR
+                                             (
+                                              @Request[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168}
+                                             )
+                                            )
+                                            AND
+                                            (
+                                             (
+                                              !(ActionMatches{'Microsoft.Authorization/roleAssignments/delete'})
+                                             )
+                                             OR
+                                             (
+                                              @Resource[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidNotEquals {8e3af657-a8ff-443c-a75c-2fe8c4bcb635, 18d7d88d-d35e-4fb5-a5c3-7773c20a72d9, f58310d9-a9f6-439a-9e8d-f62e7b41a168}
+                                             )
+                                            )
+                                            EOT
+}
