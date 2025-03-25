d634770e4c4dcfe70f85cdb5f09c59e576265fc3
Nadeen Noaman <95418928+nnoaman@users.noreply.github.com>
2025-03-25 02:47:11 +0100

    Update environment file path

--- deploy/automation/05-DB-and-SAP-installation/05-DB-and-SAP-installation-prepare.sh
+++ deploy/automation/05-DB-and-SAP-installation/05-DB-and-SAP-installation-prepare.sh
@@ -95,7 +95,7 @@ SID=$(echo "${SAP_SYSTEM_CONFIGURATION_NAME}" | awk -F'-' '{print $4}' | xargs)
 
 cd "$CONFIG_REPO_PATH" || exit
 
-environment_file_name=".sap_deployment_automation/${ENVIRONMENT}${LOCATION}${NETWORK}"
+environment_file_name="${CONFIG_REPO_PATH}/.sap_deployment_automation/${ENVIRONMENT}${LOCATION}${NETWORK}"
 parameters_filename="$CONFIG_REPO_PATH/SYSTEM/${SAP_SYSTEM_CONFIGURATION_NAME}/sap-parameters.yaml"
 
 if [[ $(get_platform) = devops ]]; then
