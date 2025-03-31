fe55880b64bed2cdfb32cc1a8de38d058676ac03
Kimmo Forss <kimforss@microsoft.com>
2025-03-31 16:50:41 +0300

    Refactor app-configuration.tf to improve dependency list formatting for clarity and consistency

--- deploy/terraform/terraform-units/modules/sap_deployer/app-configuration.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/app-configuration.tf
@@ -101,7 +101,7 @@ resource "azurerm_app_configuration_key" "deployer_keyvault_id" {
   provider                             = azurerm.main
   count                                = var.app_config_service.deploy ? 1 : 0
   depends_on                           = [
-                                            time_sleep.wait_for_appconfig_data_owner_assignment
+                                            time_sleep.wait_for_appconfig_data_owner_assignment,
                                             azurerm_private_endpoint.app_config
                                          ]
 
@@ -128,7 +128,7 @@ resource "azurerm_app_configuration_key" "deployer_resourcegroup_name" {
   provider                             = azurerm.main
   count                                = var.app_config_service.deploy ? 1 : 0
   depends_on                           = [
-                                            time_sleep.wait_for_appconfig_data_owner_assignment
+                                            time_sleep.wait_for_appconfig_data_owner_assignment,
                                             azurerm_private_endpoint.app_config
                                          ]
 
@@ -154,7 +154,7 @@ resource "azurerm_app_configuration_key" "deployer_subscription_id" {
   provider                             = azurerm.main
   count                                = var.app_config_service.deploy ? 1 : 0
   depends_on                           = [
-                                            time_sleep.wait_for_appconfig_data_owner_assignment
+                                            time_sleep.wait_for_appconfig_data_owner_assignment,
                                             azurerm_private_endpoint.app_config
                                          ]
 
@@ -180,7 +180,7 @@ resource "azurerm_app_configuration_key" "web_application_resource_id" {
   provider                             = azurerm.main
   count                                = var.app_config_service.deploy && var.app_service.use ? 1 :0
   depends_on                           = [
-                                            time_sleep.wait_for_appconfig_data_owner_assignment
+                                            time_sleep.wait_for_appconfig_data_owner_assignment,
                                             azurerm_private_endpoint.app_config
                                          ]
 
@@ -206,7 +206,7 @@ resource "azurerm_app_configuration_key" "web_application_identity_id" {
   provider                             = azurerm.main
   count                                = var.app_config_service.deploy && var.app_service.use ? 1 :0
   depends_on                           = [
-                                            time_sleep.wait_for_appconfig_data_owner_assignment
+                                            time_sleep.wait_for_appconfig_data_owner_assignment,
                                             azurerm_private_endpoint.app_config
                                          ]
 
@@ -233,7 +233,7 @@ resource "azurerm_app_configuration_key" "deployer_msi_id" {
   provider                             = azurerm.main
   count                                = var.app_config_service.deploy ? 1 : 0
   depends_on                           = [
-                                            time_sleep.wait_for_appconfig_data_owner_assignment
+                                            time_sleep.wait_for_appconfig_data_owner_assignment,
                                             azurerm_private_endpoint.app_config
                                          ]
 
