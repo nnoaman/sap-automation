34a8f97031c09f5128d47be248ccb7bdd1a5d354
Kimmo Forss <kimforss@microsoft.com>
2025-04-09 16:43:12 +0300

    Update virtual_network_id reference in appconfig_additional DNS zone link to use dns_settings.additional_network_id

--- deploy/terraform/terraform-units/modules/sap_library/infrastructure.tf
+++ deploy/terraform/terraform-units/modules/sap_library/infrastructure.tf
@@ -156,7 +156,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "vnet_mgmt_appconfig_ad
                                              azurerm_resource_group.library[0].name
                                          ))
   private_dns_zone_name                = var.dns_settings.dns_zone_names.appconfig_dns_zone_name
-  virtual_network_id                   = var.deployer_tfstate.additional_network_id
+  virtual_network_id                   = var.dns_settings.additional_network_id
   registration_enabled                 = false
   tags                                 = var.infrastructure.tags
 }
