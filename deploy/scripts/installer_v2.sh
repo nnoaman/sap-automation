8418e17f8a4baa059e3e506ef0c0dae52932ee07
Kimmo Forss <kimforss@microsoft.com>
2025-04-23 21:28:04 +0300

    Add debug echo statements in installer script for better tracing

--- deploy/scripts/installer_v2.sh
+++ deploy/scripts/installer_v2.sh
@@ -10,6 +10,7 @@ set -o pipefail
 #External helper functions
 #. "$(dirname "${BASH_SOURCE[0]}")/deploy_utils.sh"
 
+echo "0"
 script_directory="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 SCRIPT_NAME="$(basename "$0")"
 
@@ -218,7 +219,7 @@ function parse_arguments() {
 	if [ -n "$CONTROL_PLANE_NAME" ]; then
 		deployer_tfstate_key="${CONTROL_PLANE_NAME}-INFRASTRUCTURE.terraform.tfstate"
 	fi
-echo "1"
+
 	if [ "${deployment_system}" == sap_system ] || [ "${deployment_system}" == sap_landscape ]; then
 		WORKLOAD_ZONE_NAME=$(echo $parameterfile_name | cut -d'-' -f1-3)
 		if [ -n "$WORKLOAD_ZONE_NAME" ]; then
@@ -523,7 +524,7 @@ function sdaf_installer() {
 	WORKLOAD_ZONE_NAME=""
 	local green="\e[0;32m"
 	local reset="\e[0m"
-
+echo "00"
 	# Define an array of helper scripts
 	helper_scripts=(
 		"${script_directory}/helpers/script_helpers.sh"
