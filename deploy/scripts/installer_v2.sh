b113b7dabf509802a9e4d71aaf9f00bdcfec3911
Kimmo Forss <kimforss@microsoft.com>
2025-04-23 20:52:07 +0300

    Remove unnecessary whitespace and add debug echo in parse_arguments function

--- deploy/scripts/installer_v2.sh
+++ deploy/scripts/installer_v2.sh
@@ -24,7 +24,6 @@ if [[ "${DEBUG:-false}" == 'true' ]]; then
 	printenv | sort
 fi
 
-
 if printenv "CONFIG_REPO_PATH"; then
 	CONFIG_DIR="${CONFIG_REPO_PATH}/.sap_deployment_automation"
 else
@@ -219,7 +218,7 @@ function parse_arguments() {
 	if [ -n "$CONTROL_PLANE_NAME" ]; then
 		deployer_tfstate_key="${CONTROL_PLANE_NAME}-INFRASTRUCTURE.terraform.tfstate"
 	fi
-
+echo "1"
 	if [ "${deployment_system}" == sap_system ] || [ "${deployment_system}" == sap_landscape ]; then
 		WORKLOAD_ZONE_NAME=$(echo $parameterfile_name | cut -d'-' -f1-3)
 		if [ -n "$WORKLOAD_ZONE_NAME" ]; then
@@ -228,7 +227,6 @@ function parse_arguments() {
 			export TF_VAR_landscape_tfstate_key
 		else
 			WORKLOAD_ZONE_NAME=$(echo $landscape_tfstate_key | cut -d'-' -f1-3)
-
 			if [ -z $WORKLOAD_ZONE_NAME ] && [ -n "$landscape_tfstate_key" ]; then
 				WORKLOAD_ZONE_NAME=$(echo $landscape_tfstate_key | cut -d'-' -f1-3)
 			fi
