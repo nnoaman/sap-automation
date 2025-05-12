c356f37212bb84c2c35ff837a5c7b67064a95220
Kimmo Forss <kimforss@microsoft.com>
2025-05-12 10:44:05 +0300

    Update azurerm provider version to 4.28.0 across multiple Terraform files

--- deploy/terraform/run/sap_library/providers.tf
+++ deploy/terraform/run/sap_library/providers.tf
@@ -120,7 +120,7 @@ terraform                              {
                                                                          }
                                                               azurerm =  {
                                                                            source  = "hashicorp/azurerm"
-                                                                           version = "4.26.0"
+                                                                           version = "4.28.0"
                                                                          }
                                                             }
                                        }
