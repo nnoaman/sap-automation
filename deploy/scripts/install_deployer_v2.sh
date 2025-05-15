d9165980a34a639f028677bff88fd8ad4be0364d
Nadeen Noaman <95418928+nnoaman@users.noreply.github.com>
2025-05-15 14:59:18 +0200

    Check for TEST_ONLY variable using -v in install_deployer function

--- deploy/scripts/install_deployer_v2.sh
+++ deploy/scripts/install_deployer_v2.sh
@@ -366,7 +366,7 @@ function install_deployer() {
 		rm plan_output.log
 	fi
 
-	if [ "${TEST_ONLY}" == "True" ]; then
+	if [[ -v TEST_ONLY ]]; then
 		print_banner "$banner_title" "Running plan only. No deployment performed." "info"
 		exit 10
 	fi
