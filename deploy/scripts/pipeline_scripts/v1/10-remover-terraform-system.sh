f6518516eb681b031d240263f79708d9f78fc5cd
Kimmo Forss <kimforss@microsoft.com>
2025-05-15 18:57:21 +0300

    Refactor application configuration check to use -v for variable existence and add logging for querying application configuration

--- deploy/scripts/pipeline_scripts/v1/10-remover-terraform-system.sh
+++ deploy/scripts/pipeline_scripts/v1/10-remover-terraform-system.sh
@@ -38,6 +38,7 @@ export DEBUG
 set -eu
 
 if [ -v APPLICATION_CONFIGURATION_NAME ]; then
+  echo "Quering for application configuration '$APPLICATION_CONFIGURATION_NAME'"
 	APPLICATION_CONFIGURATION_ID=$(az graph query -q "Resources | join kind=leftouter (ResourceContainers | where type=='microsoft.resources/subscriptions' | project subscription=name, subscriptionId) on subscriptionId | where name == '$APPLICATION_CONFIGURATION_NAME' | project id, name, subscription" --query data[0].id --output tsv)
 fi
 
