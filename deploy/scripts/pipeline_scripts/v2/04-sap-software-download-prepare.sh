3f2dfe8633f0f278a0723a42d651140371910dc4
Nadeen Noaman <95418928+nnoaman@users.noreply.github.com>
2025-05-22 13:24:15 +0200

    Add orchestration_ansible_user parameter for GitHub platform configuration

--- deploy/scripts/pipeline_scripts/v2/04-sap-software-download-prepare.sh
+++ deploy/scripts/pipeline_scripts/v2/04-sap-software-download-prepare.sh
@@ -194,6 +194,7 @@ elif [ "$PLATFORM" == "github" ]; then
 		-e bom_base_name='${BOM}' \
 		-e deployer_kv_name=${key_vault} \
 		-e check_storage_account=${re_download} \
+		-e orchestration_ansible_user=root \
 		${EXTRA_PARAMETERS} \
 		${SAP_AUTOMATION_REPO_PATH}/deploy/ansible/playbook_bom_downloader.yaml"
 	echo "Executing [$command]"
