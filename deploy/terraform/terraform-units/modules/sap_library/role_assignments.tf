45872a3e3f5e25cb0bbac72412ce2ace800dbc11
Kimmo Forss <kimforss@microsoft.com>
2025-06-03 22:00:59 +0300

    Update role assignments to use "Role Based Access Control Administrator" and add conditions for access control

--- deploy/terraform/terraform-units/modules/sap_library/role_assignments.tf
+++ deploy/terraform/terraform-units/modules/sap_library/role_assignments.tf
@@ -95,7 +95,7 @@ resource "azurerm_role_assignment" "resource_group_user_access_admin_spn" {
                                                  data.azurerm_resource_group.library[0].id) : (
                                                  azurerm_resource_group.library[0].id
                                                )
-  role_definition_name                 = "User Access Administrator"
+  role_definition_name                 = "Role Based Access Control Administrator"
   principal_type                       = "ServicePrincipal"
   principal_id                         = var.infrastructure.spn_id
   condition_version                    = "2.0"
