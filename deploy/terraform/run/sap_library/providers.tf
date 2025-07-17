516874481c38ff990fdc132cfdfcab0b8bbd7324
Kimmo Forss <kimforss@microsoft.com>
2025-07-17 22:18:34 +0300

    fix: Remove trailing comma in azuread provider configuration for cleaner syntax

--- deploy/terraform/run/sap_library/providers.tf
+++ deploy/terraform/run/sap_library/providers.tf
@@ -94,8 +94,8 @@ provider "azurerm"                     {
                                        }
 
 provider "azuread"                     {
-                                         client_id                  = var.use_spn ? coalesce(data.azurerm_key_vault_secret.client_id_v2[0].value,data.azurerm_key_vault_secret.client_id[0].value) : null,
-                                         client_secret              = var.use_spn ? coalesce(ephemeral.azurerm_key_vault_secret.client_secret_v2[0].value, ephemeral.azurerm_key_vault_secret.client_secret[0].value) : null,
+                                         client_id                  = var.use_spn ? coalesce(data.azurerm_key_vault_secret.client_id_v2[0].value,data.azurerm_key_vault_secret.client_id[0].value) : null
+                                         client_secret              = var.use_spn ? coalesce(ephemeral.azurerm_key_vault_secret.client_secret_v2[0].value, ephemeral.azurerm_key_vault_secret.client_secret[0].value) : null
                                          tenant_id                  = var.use_spn ? coalesce(data.azurerm_key_vault_secret.tenant_id_v2[0].value,data.azurerm_key_vault_secret.tenant_id[0].value) : null
                                        }
 
