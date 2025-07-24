14c6a8c68eacfc74babf186cc2d66afca93bb35a
Kimmo Forss <kimforss@microsoft.com>
2025-07-25 00:39:37 +0300

    feat: Enhance debug logging and execution environment details in control plane permission script

--- deploy/scripts/pipeline_scripts/01-control-plane-permission-pipelines.sh
+++ deploy/scripts/pipeline_scripts/01-control-plane-permission-pipelines.sh
@@ -7,14 +7,33 @@ script_directory="$(dirname "${full_script_path}")"
 parent_directory="$(dirname "$script_directory")"
 SCRIPT_NAME="$(basename "$0")"
 
+#call stack has full script name when using source
+source "${parent_directory}/helper.sh"
 source "${parent_directory}/deploy_utils.sh"
 set -e
 
 return_code=0
+print_banner "$banner_title" "Starting $SCRIPT_NAME" "info"
+
+DEBUG=False
+
+if [ "$SYSTEM_DEBUG" = True ]; then
+	set -x
+	DEBUG=True
+	echo "Environment variables:"
+	printenv | sort
+
+fi
+export DEBUG
+set -eu
+
+# Print the execution environment details
+print_header
 
 # Configure DevOps
 configure_devops
 
+
 az pipelines list --query "[].{id:id, name:name}" -o table
 
-exit $return_code
+exit 10
