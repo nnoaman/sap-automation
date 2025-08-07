91bb162fee888c3b6cc9626c2c3570306bc5ffe1
Kimmo Forss <kimforss@microsoft.com>
2025-08-08 01:19:20 +0300

    fix: Update web app configuration script to correctly reference APP_REGISTRATION_OBJECTID and APP_SERVICE_NAME variables

--- deploy/scripts/pipeline_scripts/01-webapp-configuration.sh
+++ deploy/scripts/pipeline_scripts/01-webapp-configuration.sh
@@ -29,13 +29,13 @@ printf "az ad app update --id %s --web-home-page-url https://%s.azurewebsites.ne
 
 printf "\n" >>"$BUILDPATH/Web Application Configuration.md"
 
-printf "az rest --method POST --uri \"https://graph.microsoft.com/beta/applications/%s/federatedIdentityCredentials\" --body \"{'name': 'ManagedIdentityFederation', 'issuer': 'https://login.microsoftonline.com/%s/v2.0', 'subject': '%s', 'audiences': [ 'api://AzureADTokenExchange' ]}\"" $(APP_REGISTRATION_OBJECTID) $(ARM_TENANT_ID) $(ARM_OBJECT_ID) >>"$BUILDPATH/Web Application Configuration.md"
+printf "az rest --method POST --uri \"https://graph.microsoft.com/beta/applications/%s/federatedIdentityCredentials\" --body \"{'name': 'ManagedIdentityFederation', 'issuer': 'https://login.microsoftonline.com/%s/v2.0', 'subject': '%s', 'audiences': [ 'api://AzureADTokenExchange' ]}\"" $APP_REGISTRATION_OBJECTID $ARM_TENANT_ID $ARM_OBJECT_ID >>"$BUILDPATH/Web Application Configuration.md"
 printf "\n" >>"$BUILDPATH/Web Application Configuration.md"
 
 printf "az webapp restart --ids %s\n\n" "$app_resource_id" >>"$BUILDPATH/Web Application Configuration.md"
 printf "\n\n" >>"$BUILDPATH/Web Application Configuration.md"
 
-printf "[Access the Web App](https://%s.azurewebsites.net) \n\n" $(APP_SERVICE_NAME) >>"$BUILDPATH/Web Application Configuration.md"
+printf "[Access the Web App](https://%s.azurewebsites.net) \n\n" $APP_SERVICE_NAME >>"$BUILDPATH/Web Application Configuration.md"
 
 echo "##vso[task.uploadsummary]$BUILDPATH/Web Application Configuration.md"
 exit 0
