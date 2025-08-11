c3557e79cb80c69277fce75f2f0ec4b3ec8609bf
Kimmo Forss <kimforss@microsoft.com>
2025-08-11 18:08:05 +0300

    fix: Update count condition for appconfig_agent resource to include additional_network_id check

--- deploy/terraform/terraform-units/modules/sap_library/app-configuration.tf
+++ deploy/terraform/terraform-units/modules/sap_library/app-configuration.tf
@@ -197,7 +197,10 @@ resource "azurerm_private_dns_zone_virtual_network_link" "vnet_mgmt_appconfig_ad
 
 resource "azurerm_private_dns_zone_virtual_network_link" "appconfig_agent" {
   provider                             = azurerm.dnsmanagement
-  count                                = var.dns_settings.register_storage_accounts_keyvaults_with_dns && var.use_private_endpoint && length(var.dns_settings.additional_network_id) > 0 ? 1 : 0
+  count                                = ( var.dns_settings.register_storage_accounts_keyvaults_with_dns &&
+                                           var.use_private_endpoint &&
+                                           length(var.dns_settings.additional_network_id) > 0 &&
+                                           var.dns_settings.additional_network_id != var.deployer_tfstate.additional_network_id) ? 1 : 0
   depends_on                           = [
                                             azurerm_private_dns_zone.vault
                                          ]
