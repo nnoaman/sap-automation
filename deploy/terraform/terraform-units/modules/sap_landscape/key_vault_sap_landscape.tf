6305de2868e061364f674f96cd06f95b8af58b86
Kimmo Forss <kimforss@microsoft.com>
2025-08-12 14:57:30 +0300

    Fix formatting of conditionals in role assignment resources for consistency

--- deploy/terraform/terraform-units/modules/sap_landscape/key_vault_sap_landscape.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/key_vault_sap_landscape.tf
@@ -99,7 +99,7 @@ resource "azurerm_management_lock" "keyvault" {
 
 resource "azurerm_role_assignment" "role_assignment_msi" {
   provider                             = azurerm.deployer
-  count                                = var.enable_rbac_authorization_for_keyvault && var.options.assign_permissions && contains(keys(var.deployer_tfstate,"deployer_uai")) ? 1 : 0
+  count                                = var.enable_rbac_authorization_for_keyvault && var.options.assign_permissions && contains(keys(var.deployer_tfstate),"deployer_uai") ? 1 : 0
   scope                                = var.key_vault.user.exists ? (
                                            data.azurerm_key_vault.kv_user[0].id) : (
                                            azurerm_key_vault.kv_user[0].id
@@ -115,7 +115,7 @@ resource "azurerm_role_assignment" "role_assignment_msi" {
 
 resource "azurerm_role_assignment" "role_assignment_msi_officer" {
   provider                             = azurerm.deployer
-  count                                = var.enable_rbac_authorization_for_keyvault && var.options.assign_permissions && contains(keys(var.deployer_tfstate,"deployer_uai")) ? 1 : 0
+  count                                = var.enable_rbac_authorization_for_keyvault && var.options.assign_permissions && contains(keys(var.deployer_tfstate),"deployer_uai") ? 1 : 0
   scope                                = var.key_vault.user.exists ? (
                                            data.azurerm_key_vault.kv_user[0].id) : (
                                            azurerm_key_vault.kv_user[0].id
