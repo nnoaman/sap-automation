96cf87cf1ff3cc2f680996537e7d05b5f1a5f6ee
Kimmo Forss <kimforss@microsoft.com>
2025-08-12 14:58:55 +0300

    Fix variable reference in role assignment count condition

--- deploy/terraform/terraform-units/modules/sap_landscape/role_assignments.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/role_assignments.tf
@@ -1,6 +1,6 @@
 resource "azurerm_role_assignment" "resource_group_user_access_admin" {
   provider                             = azurerm.deployer
-  count                                = var.infrastructure.assign_permissions && contains(keys(var.deployer_tfstate),"deployer_uai") ? 1 : 0
+  count                                = var.options.assign_permissions && contains(keys(var.deployer_tfstate),"deployer_uai") ? 1 : 0
   scope                                = local.resource_group_exists ? (
                                                                    data.azurerm_resource_group.resource_group[0].id) : (
                                                                    try(azurerm_resource_group.resource_group[0].id, "")
