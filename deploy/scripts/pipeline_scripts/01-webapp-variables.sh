bf4a6722443061f25d5192c661c2716c1dfc2746
Kimmo Forss <kimforss@microsoft.com>
2025-08-13 00:10:32 +0300

    fix: correct variable expansion for HAS_WEBAPP output

--- deploy/scripts/pipeline_scripts/01-webapp-variables.sh
+++ deploy/scripts/pipeline_scripts/01-webapp-variables.sh
@@ -2,6 +2,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-echo "##vso[task.setvariable variable=HAS_WEBAPP;isOutput=true]$(HAS_APPSERVICE_DEPLOYED)"
+echo "##vso[task.setvariable variable=HAS_WEBAPP;isOutput=true]$HAS_APPSERVICE_DEPLOYED"
 
 exit 0
