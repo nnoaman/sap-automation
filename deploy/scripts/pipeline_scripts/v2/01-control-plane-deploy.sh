0978228073f6ff0e4b931b141a2111e445eeadfc
Nadeen Noaman <95418928+nnoaman@users.noreply.github.com>
2025-08-22 17:05:58 +0200

    fix: add ZONE environment variable configuration for GitHub Actions

--- deploy/scripts/pipeline_scripts/v2/01-control-plane-deploy.sh
+++ deploy/scripts/pipeline_scripts/v2/01-control-plane-deploy.sh
@@ -86,6 +86,7 @@ elif [ "$PLATFORM" == "github" ]; then
 	# Values will be stored in GitHub Environment variables
 	echo "Configuring for GitHub Actions"
 	export VARIABLE_GROUP_ID="${CONTROL_PLANE_NAME}"
+	export ZONE="${CONTROL_PLANE_NAME}"
 	git config --global --add safe.directory "$CONFIG_REPO_PATH"
 
 	# Set required environment variables for GitHub
