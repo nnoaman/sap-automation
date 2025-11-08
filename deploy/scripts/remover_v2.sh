b039e6d473250f1eecaa9876d4d6b1299a6c34ad
Kimmo Forss <kimforss@microsoft.com>
2025-11-09 01:48:14 +0200

    Add color formatting variables for improved script output readability

--- deploy/scripts/remover_v2.sh
+++ deploy/scripts/remover_v2.sh
@@ -2,6 +2,12 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+green="\e[1;32m"
+bold_red="\e[1;31m"
+cyan="\e[1;36m"
+bold_red_underscore="\e[1;4;31m"
+reset_formatting="\e[0m"
+reset="\e[0m"
 
 # Ensure that the exit status of a pipeline command is non-zero if any
 # stage of the pipefile has a non-zero exit status.
