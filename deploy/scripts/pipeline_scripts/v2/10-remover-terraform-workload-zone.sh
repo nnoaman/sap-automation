6518fa50342fbec395eacc3f7706faee78edeed3
Kimmo Forss <kimforss@microsoft.com>
2025-11-14 12:00:30 +0200

    Update tfvars file path and environment variable extraction in workload zone script

--- deploy/scripts/pipeline_scripts/v2/10-remover-terraform-workload-zone.sh
+++ deploy/scripts/pipeline_scripts/v2/10-remover-terraform-workload-zone.sh
@@ -65,7 +65,9 @@ fi
 
 print_banner "$banner_title" "Entering $SCRIPT_NAME" "info"
 
-tfvarsFile="LANDSCAPE/${WORKLOAD_ZONE_FOLDERNAME}/${WORKLOAD_ZONE_TFVARS_FILENAME}"
+
+tfvarsFile="$CONFIG_REPO_PATH/LANDSCAPE/$WORKLOAD_ZONE_NAME-INFRASTRUCTURE/${WORKLOAD_ZONE_NAME}-INFRASTRUCTURE.tfvars"
+
 
 cd "${CONFIG_REPO_PATH}" || exit
 # Platform-specific git checkout
@@ -127,10 +129,10 @@ ENVIRONMENT=$(grep -m1 "^environment" "$tfvarsFile" | awk -F'=' '{print $2}' | t
 LOCATION=$(grep -m1 "^location" "$tfvarsFile" | awk -F'=' '{print $2}' | tr '[:upper:]' '[:lower:]' | tr -d ' \t\n\r\f"')
 NETWORK=$(grep -m1 "^network_logical_name" "$tfvarsFile" | awk -F'=' '{print $2}' | tr -d ' \t\n\r\f"')
 
-ENVIRONMENT_IN_FILENAME=$(echo ${WORKLOAD_ZONE_NAME}-INFRASTRUCTURE | awk -F'-' '{print $1}')
-LOCATION_CODE_IN_FILENAME=$(echo ${WORKLOAD_ZONE_NAME}-INFRASTRUCTURE | awk -F'-' '{print $2}')
+ENVIRONMENT_IN_FILENAME=$(echo ${WORKLOAD_ZONE_NAME} | awk -F'-' '{print $1}')
+LOCATION_CODE_IN_FILENAME=$(echo ${WORKLOAD_ZONE_NAME} | awk -F'-' '{print $2}')
 LOCATION_IN_FILENAME=$(get_region_from_code "$LOCATION_CODE_IN_FILENAME" || true)
-NETWORK_IN_FILENAME=$(echo ${WORKLOAD_ZONE_NAME}-INFRASTRUCTURE | awk -F'-' '{print $3}')
+NETWORK_IN_FILENAME=$(echo ${WORKLOAD_ZONE_NAME} | awk -F'-' '{print $3}')
 
 landscape_tfstate_key="${WORKLOAD_ZONE_NAME}-INFRASTRUCTURE.terraform.tfstate"
 export landscape_tfstate_key
